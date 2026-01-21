6abe55b6178831d9fc1d03792f44843566da2c0b
Rustam Patel <53313031+rustamsinghpatel@users.noreply.github.com>
2026-01-21 23:48:15 +0530

    Hero section designed with responsive layout and animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-console.log("E-Commerce Website Loaded");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
